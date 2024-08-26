ec5c6025b44767992d5e03410b5f49346afa9437
omar <baiomayomar@gmail.com>
2024-08-26 04:14:44 +0300

    update web app

--- app.py
+++ app.py
@@ -3,8 +3,22 @@
 from flask import Flask, render_template
 from flask_scss import Scss
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
+Scss(app)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite://todoapp.db"
+db = SQLAlchemy(app)
+
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False)
+    completed = db.Column(db.Integer, default=0)
+    created = db.Column(db.datetime, default=datetime.utcnow)
+    
+    def __repr__(self) -> str:
+        return f"Task {self.id}"
 
 
 @app.route("/")
@@ -13,4 +27,7 @@ def index():
 
 
 if __name__ in "__main__":
+    with app.app_context():
+        db.create_all()
+    
     app.run(debug=True)
